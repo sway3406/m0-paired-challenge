e3132c0a0650c846b1d3459d8ae0650e2383ec76
Shane Way <shane.c.way2@gmail.com>
2022-04-18 21:39:36 -0600

    Initial commit

---
+++ challenge/index.md
@@ -0,0 +1,15 @@
+## 5 of My _Favorite_ Books
+
+### List of Books
+
+- Book 1
+- Book 2
+- Book 3
+- Book 4
+- Book 5
+
+#### Workflow
+
+1) For our paired work, Emma and I worked in tandem based on the given instructions by the teacher.
+Emma and I shared thoughts and problem solved through a slight mistake we made by not creating a
+`.js file` and coding in a `.md file`. Once we sorted this issue out we continued on with the directions. As for our workflow, we worked line by line together until time elapsed and we needed to rejoin the main group. We finished approximately 50% of the work in the given time.
---
+++ challenge/paired.challenge.js
@@ -0,0 +1,11 @@
+// File name was chosen to match up with the forked repository title.
+
+
+var bookStore = 'My Little 5 Story Library';   // Title of Library
+var numTotalBooks = 500;
+var booksCheckedIn = 400
+var booksCheckedOut = numTotalBooks - booksCheckedIn;  // Calculates total books checked out by customers
+var booksAvailable = true;  // If books checked out is less than total, prompt will allow book check outs.
+var checkOutNote = 'Thanks for checking out a book! Your book is due back in 10 days!' ; // Thank you note & due date reminder.
+
+var libraryFloors = ['floor 1', 'floor 2', 'floor 3', 'floor 4', 'floor 5'];
